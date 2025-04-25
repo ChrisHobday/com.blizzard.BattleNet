54b19fe9ccd9a021c3bbb0c803ef43a11248da8e
Chris Hobday <hobday.chris@protonmail.com>
2025-04-24 21:55:03 -0400

    Move wineboot, regedit and Discord setup so that they are run every time

--- Scripts/BattleNet.sh
+++ Scripts/BattleNet.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+echo "Running Wineboot"
+wineboot
+
+echo "Updating registry"
+wine regedit /app/share/WineGraphics.reg
+sleep 3
+
+echo "Setting up Discord rich presence"
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i ||
+    ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 echo "Checking if Battle.net is already installed"
 if [ -d "/var/data/wine/drive_c/Program Files (x86)/Battle.net/" ]; then
   echo "Battle.net is installed"
-
-  echo "Setting up Discord rich presence"
-  for i in {0..9}; do
-      test -S $XDG_RUNTIME_DIR/discord-ipc-$i ||
-      ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
-  done
 
   echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Battle.net/"
   cd /var/data/wine/drive_c/Program\ Files\ \(x86\)/Battle.net/
@@ -17,15 +24,6 @@ if [ -d "/var/data/wine/drive_c/Program Files (x86)/Battle.net/" ]; then
 else
   echo "Battle.net is not installed"
 
-  echo "Initializing Wine prefix"
-  wineboot -i
-
-  echo "Updating Windows registry with Wine graphics keys"
-  wine regedit /app/share/WineGraphics.reg
-
-  echo "Waiting for registry updates to be applied"
-  sleep 3
-
-  echo "Launching BattleNetSetup.exe"
+  echo "Launching Battle.net setup"
   wine /app/bin/BattleNetSetup.exe
 fi
